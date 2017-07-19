559d9d82c1278b6b11c284c7f7b3182415914bc0
caristopmer <chris.mcmenomy@gmail.com>
2017-07-18 23:17:56 -0500

    user register, login, logout working.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: redirect('/songs')
-  get '/songs' => 'songs#index'
+  get '/songs' => 'songs#index', as: "songs"
+  get '/users/new' => 'users#new', as: "new_user"
+  post '/users' => 'users#create'
+  delete '/sessions' => 'sessions#destroy'
+  get '/sessions/new' => 'sessions#new', as: "new_session"
+  post '/sessions' => 'sessions#create'
 end
